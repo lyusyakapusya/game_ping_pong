f9260ff5204a4889893870cff36723a43ce960e2
Lyusya Kapusya <135048785+lyusyakapusya@users.noreply.github.com>
2023-06-02 19:54:22 +0300

    Update my_pingpong.py
    dell pygame

--- my_pingpong.py
+++ my_pingpong.py
@@ -1,117 +1,73 @@
-import pygame
+from pygame import *
 from random import randint
-pygame.init()
 
 
+clock = time.Clock()
 back = (200, 255, 255) #цвет фона (background)
-mw = pygame.display.set_mode((500, 500)) #окно программы (main window)
+mw = display.set_mode((500, 500)) #окно программы (main window)
 mw.fill(back)
-
-racket_x = 200
-racket_y = 330
-
-speed_x = 4
-speed_y = 4
-
+clock = time.Clock()
 game_over = False
-moving_right = False
-moving_left = False
 
-#end_flag
-game_over = False
 
 '''класс прямоугльник'''
  
 class Area():
     def __init__(self, x=0, y=0, width=10, height=10, color=None): #его характериститки
+        self.rect = Rect(x, y, width, height) #прямоугольник
+        self.fill_color = back
+        if color:
+            self.fill_color = color
+    def update(self):
+        keys_passed = key.get_pressed()
+        if keys_passed[K_UP] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys_passed[K_DOWN] and self.rect.x < win_width -80:
+            self.rect.x += self.speed
+
+ 
+    def color(self, new_color):
+        self.fill_color = new_color
+ 
+ 
+    def fill(self):
+        pygame.draw.rect(mw, self.fill_color, self.rect)
+        
+    def collidepoint(self,x, y):
+        return self.rect.collidepoint(x, y)
+
+    def collidepoint(self, rect):
+        return self.rect.collidepoint(rect)
 
 
 '''класс мячик'''
 
 class Picture(Area):
-    def __init__(self, filename, x = 0, y = 0, width=10, height=10): #его характеристики
+    def __init__(self, filename, x = 0, y = 0, width=10, height=10):
+        Area.__init__(self, x = x, y=y, width=width, height=height, color = None)
+        self.image = image.load(filename) 
+
+    def draw(self):
+        mw.blit(self.image, (self.rect.x, self.rect.y))
+#его характеристики
 
 
 ball = Picture('ball.png', 160, 200, 50, 50) #мячик
-platform_1 = Picture('platform.png',racket_x, racket_y, 100, 30) #платформа 1
-platform_2 = Picture('platform.png',racket_x, racket_y, 100, 30) #платформа 2
+platform_1 = Picture('platform.png', 0, 0, 100, 30) #платформа 1
+platform_2 = Picture('platform.png',0, 0, 100, 20) #платформа 2
 
-
-start_x = 5
-start_y = 5
-count = 9
-monsters = []
-
-
-
-for i in range(3):
-    y = start_x + (55 *i)
-    x = start_y + (27.5 * i)
-
-    for i in range(count):
-        g = Picture('enemy.png', x, y, 50, 50)
-        monsters.append(g)
-        x = x+ 55
-
-    count = count - 1
 
 while not game_over:
+    for e in event.get():
+        if e.type == QUTI:
+            game = False
     ball.fill()
-    platform.fill()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
-        
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                moving_right = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                moving_left = True
-
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                moving_right = False
-            if event.key == pygame.K_LEFT:
-                moving_left = False
+    platform_1.fill()
+    platform_2.fill()
 
 
-
-    if moving_left:
-        platform.rect.x -=5
-
-    if moving_right:
-        platform.rect.x +=5
-
-    ball.rect.x += speed_x
-    ball.rect.y += speed_y
-
-    if ball.rect.y < 0:
-        speed_y *= -1
-    if ball.rect.x > 450 or ball.rect.x < 0:
-        speed_x *= -1
-
-    if ball.rect.colliderect(platform.rect):
-        speed_y *= -1
-
-    if ball.rect.y > 350 or monsters==[]:
-        lose_game.fill()
-        lose_game.draw()
-        game_over = True
-
-    for monster in monsters:
-        monster.draw()
-        if monster.rect.colliderect(ball.rect):
-            monsters.remove(monster)
-            monster.fill()
-            speed_y *= -1
-
-    platform.draw()
-    ball.draw()
-
-
-
-
-    pygame.display.update()
-    clock.tick(40)
+''' вырисовываем мачик и платформы'''
+platform_1.draw()
+platform_2.draw()
+ball.draw()
+pygame.display.update()
